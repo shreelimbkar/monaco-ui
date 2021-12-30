77945c7ffbdb72518e4baaef27793275cf426b03
shreelimbkar <limbkarshree@gmail.com>
2021-12-30 23:28:32 +0000

    updated docs files

--- packages/docs/.docz/.cache/sync-requires.js
+++ packages/docs/.docz/.cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": (preferDefault(require("/Users/shrivardhanlimbkar/Personal/git-repos/monaco-ui/packages/docs/.docz/.cache/dev-404-page.js"))),
   "component---src-components-box-mdx": (preferDefault(require("/Users/shrivardhanlimbkar/Personal/git-repos/monaco-ui/packages/docs/src/components/box.mdx"))),
   "component---src-components-button-mdx": (preferDefault(require("/Users/shrivardhanlimbkar/Personal/git-repos/monaco-ui/packages/docs/src/components/button.mdx"))),
   "component---src-index-mdx": (preferDefault(require("/Users/shrivardhanlimbkar/Personal/git-repos/monaco-ui/packages/docs/src/index.mdx"))),
